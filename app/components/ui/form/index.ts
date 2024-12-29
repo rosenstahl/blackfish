94d4078d5434a0658e3b47a9a31da3ca2b8d786d
rosenstahl <hasanyilmaz1811@gmail.com>
2024-12-29 19:26:02 +0100

    feat: add form components barrel file

---
+++ app/components/ui/form/index.ts
@@ -0,0 +1,13 @@
+export { Form } from './Form'
+export { Input } from './Input'
+export { Textarea } from './Textarea'
+export { Select } from './Select'
+export { Checkbox } from './Checkbox'
+export { RadioGroup } from './Radio'
+
+// Re-export types
+export type { InputProps } from './Input'
+export type { TextareaProps } from './Textarea'
+export type { SelectProps } from './Select'
+export type { CheckboxProps } from './Checkbox'
+export type { RadioGroupProps, RadioOption } from './Radio'
