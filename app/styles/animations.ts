076e0a840e612c4b2787caeaf24182dc2152c1cd
rosenstahl <hasanyilmaz1811@gmail.com>
2025-01-01 17:36:28 +0100

    feat: add animation styles

---
+++ app/styles/animations.ts
@@ -0,0 +1,29 @@
+import { MotionProps } from 'framer-motion'
+
+export const menuAnimation: MotionProps = {
+  initial: { opacity: 0, y: -10 },
+  animate: { opacity: 1, y: 0 },
+  exit: { opacity: 0, y: -10 },
+  transition: { duration: 0.2 }
+}
+
+export const fadeAnimation: MotionProps = {
+  initial: { opacity: 0 },
+  animate: { opacity: 1 },
+  exit: { opacity: 0 },
+  transition: { duration: 0.2 }
+}
+
+export const slideUpAnimation: MotionProps = {
+  initial: { opacity: 0, y: 20 },
+  animate: { opacity: 1, y: 0 },
+  exit: { opacity: 0, y: 20 },
+  transition: { duration: 0.3 }
+}
+
+export const slideInAnimation: MotionProps = {
+  initial: { opacity: 0, x: 20 },
+  animate: { opacity: 1, x: 0 },
+  exit: { opacity: 0, x: 20 },
+  transition: { duration: 0.3 }
+}
